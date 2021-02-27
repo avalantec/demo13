ada6d1203e8db8d4d111795829f63c593e929858
jbarahona2095 <jbarahona2095@hotmail.com>
2021-02-27 14:48:46 -0600

    check

--- checks/__manifest__.py
+++ checks/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "hr_recruitment_no_header_footer",
+    'name': "checks",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -10,26 +10,22 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Avalantec",
+    'website': "https://www.avalantec.com/",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'account_accountant',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','website','hr_recruitment'],
+    'depends': ['base','account','l10n_us_check_printing'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'Report/report_check_top.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+
 }
