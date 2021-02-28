44cf209661bcb7c678a45cb07fc261cf90bb22be
jbarahona2095 <jbarahona2095@hotmail.com>
2021-02-28 15:29:53 -0600

    cheques lines

--- format_checks/models/models.py
+++ format_checks/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import hashlib 
 
 class AccountJournal(models.Model):
     _inherit = 'account.journal'
@@ -10,14 +11,36 @@ class AccountJournal(models.Model):
 
     bank_name = fields.Char(string='Bank name')
     bank_address = fields.Char(string='Bank Address')
+    hash_code = fields.Char(string='hash',readonly=True)
+
+    def create_hash_code(self):
+        data = self.company_name+self.company_address+self.bank_name+self.bank_address
+        return hashlib.md5(data.encode()).hexdigest() 
 
     def create(self, values):
+        hash_code = self.create_hash_code()
+        if self.hash_code != hash_code:
+            values['hash_code'] = hash_code
+            self.log_in_chatter(hash_code)
         res = super(AccountJournal, self).create(values)
         return res
 
     def write(self, values):
-        res = super(AccountJournal, self).write()
+        hash_code = self.create_hash_code()
+        if self.hash_code != hash_code:
+            values['hash_code'] = hash_code
+            self.log_in_chatter(hash_code)
+        res = super(AccountJournal, self).write(values)
         return res
+    
+    def log_in_chatter(self,body):
+        chatter = self.env['mail.message']
+        chatter.create({
+                        'res_id': self.id,
+                        'model':'account.journal',
+                        'body': body,
+                       })
+
 
     
 
