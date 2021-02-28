3c808a0a6b00db9f6f2af0409631b67d2f6d22c5
jbarahona2095 <jbarahona2095@hotmail.com>
2021-02-28 15:36:29 -0600

    cheques lines

--- format_checks/models/models.py
+++ format_checks/models/models.py
@@ -2,6 +2,8 @@
 
 from odoo import models, fields, api
 import hashlib 
+import time
+log = logging.getLogger(__name__)
 
 class AccountJournal(models.Model):
     _inherit = 'account.journal'
@@ -14,10 +16,11 @@ class AccountJournal(models.Model):
     hash_code = fields.Char(string='hash',readonly=True)
 
     def create_hash_code(self):
-        data = self.company_name+self.company_address+self.bank_name+self.bank_address
-        return hashlib.md5(data.encode()).hexdigest() 
+        data = self.company_name + self.company_address + self.bank_name + self.bank_address
+        return hashlib.md5(data).hexdigest() 
 
     def create(self, values):
+        log.info("valuuuuesss {0}".format(values))
         hash_code = self.create_hash_code()
         if self.hash_code != hash_code:
             values['hash_code'] = hash_code
@@ -26,6 +29,8 @@ class AccountJournal(models.Model):
         return res
 
     def write(self, values):
+        log.info(' item name no tiene clave %s',item.name)
+        if values['id']:
         hash_code = self.create_hash_code()
         if self.hash_code != hash_code:
             values['hash_code'] = hash_code
