0deee936929eb84807a5b2cc4cb86c0b04f55b62
jbarahona2095 <jbarahona2095@hotmail.com>
2021-02-28 17:05:41 -0600

    cheques lines

--- format_checks/models/models.py
+++ format_checks/models/models.py
@@ -19,22 +19,22 @@ class AccountJournal(models.Model):
 
     def create_hash_code(self,values):
         data = ''
-        if values.get('company_name'):
+        if values.get('company_name') or  values.get('company_name') == False:
             data = data + values.get('company_name')
         else:
             data = data + str(self.company_name)
 
-        if values.get('company_address'):
+        if values.get('company_address') or values.get('company_address') == False:
             data = data + values.get('company_address')
         else:
             data = data + str(self.company_address)
 
-        if values.get('bank_name'):
+        if values.get('bank_name') or values.get('bank_name') == False:
             data = data + values.get('bank_name')
         else:
             data = data + str(self.bank_name)
 
-        if values.get('bank_address'):
+        if values.get('bank_address') or values.get('bank_address') == False:
             data = data + values.get('bank_address')
         else:
             data = data + str(self.bank_address)
